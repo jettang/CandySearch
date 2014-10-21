4320b094b6a48022562ab2d4b350e4316f91a693
sxyx2008 <sxyx2008@163.com>
2014-10-21 23:18:55 +0800

    Initial commit

---
+++ CandySearch/Candy.swift
@@ -0,0 +1,14 @@
+//
+//  Candy.swift
+//  CandySearch
+//
+//  Created by scott on 14-8-31.
+//  Copyright (c) 2014年 冯靖. All rights reserved.
+//
+
+import Foundation
+
+struct Candy{
+    let category:String
+    let name:String
+}
